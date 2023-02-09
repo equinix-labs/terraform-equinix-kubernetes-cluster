946e4a413be0ee14287c573078de4ceaee4cf184
Chris Privitere <23177737+cprivitere@users.noreply.github.com>
2023-02-09 15:47:22 -0600

    alphabatize variables
    Signed-off-by: Chris Privitere <23177737+cprivitere@users.noreply.github.com>

--- examples/cpem-add-on/variables.tf
+++ examples/cpem-add-on/variables.tf
@@ -1,52 +1,21 @@
 variable "auth_token" {
-  type        = string
-  description = "Equinix Metal API key"
-  sensitive   = true
+  sensitive = true
+  type      = string
+}
+variable "cpem_version" {
+  type    = string
+  default = "v3.5.0"
 }
 
+variable "kube_vip_version" {
+  type    = string
+  default = "v0.5.9"
+}
+variable "kubernetes_version" {
+  type    = string
+  default = "v1.26.1"
+}
 variable "project_id" {
-  type        = string
-  description = "Equinix Metal Project ID"
-}
-
-variable "metro" {
-  type        = string
-  description = "Equinix Metal Metro"
-  default     = "sv"
-}
-
-variable "ssh_key_file" {
-  type        = string
-  description = "Path to SSH key file for access to cluster nodes"
-}
-
-variable "count_x86" {
-  type        = string
-  description = "Number of x86 nodes"
-  default     = "3"
-}
-
-variable "count_arm" {
-  type        = string
-  description = "Number of ARM nodes"
-  default     = "0"
-}
-
-variable "cluster_name" {
-  type        = string
-  description = "Name of your cluster. Alpha-numeric and hyphens only, please."
-  default     = "metal-multiarch-k8s"
-}
-variable "private_key_path" {
-  type        = string
-  description = "Path to an SSH key to use for the connection"
-}
-variable "host" {
-  type        = string
-  description = "The address of the server from where to perform kubectl installations and changes"
-}
-variable "user" {
-  type        = string
-  description = "The username of the ssh key used to connect to the server defined in host"
-  default     = "root"
+  sensitive = true
+  type      = string
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,12 @@
 variable "auth_token" {
-  type = string
+  sensitive = true
+  type      = string
 }
-variable "project_id" {
-  type = string
+variable "cpem_version" {
+  type    = string
+  default = "v3.5.0"
 }
+
 variable "kube_vip_version" {
   type    = string
   default = "v0.5.9"
@@ -12,7 +15,7 @@ variable "kubernetes_version" {
   type    = string
   default = "v1.26.1"
 }
-variable "cpem_version" {
-  type    = string
-  default = "v3.5.0"
+variable "project_id" {
+  sensitive = true
+  type      = string
 }
