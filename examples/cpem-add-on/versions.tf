44f9b8a591e1ac20a3bde1856aade383c52c4db2
vasubabu <kandimalla.vasubabu@gmail.com>
2023-05-04 16:32:24 +0530

    Add provider meta module_name in Equinix Metal TF configs

--- examples/cpem-add-on/versions.tf
+++ examples/cpem-add-on/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     equinix = {
       source  = "equinix/equinix"
-      version = ">= 1.10"
+      version = "~> 1.14"
     }
   }
-  required_version = ">= 1"
+  required_version = ">= 1.0.0"
 }
